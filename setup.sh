df8c27d2e1fe58dee87583d1440577cfcc61c6e0
OadeO6 <abdurrahman.adenow06@gmail.com>
2024-06-24 16:24:03 +0000

    update

--- setup.sh
+++ setup.sh
@@ -4,17 +4,57 @@
 
 dependencies(){
     # python
-    # apt install -y python3
-    apt install python3.12-venv
+    if ( which python 1>/dev/null || which python3 1> /dev/null)
+    then
+	    echo "python installed"
+    else
+	    echo "-- Installing python......"
+	    apt install -y python3
+    fi
+
+    #apt install -y python3.12-venv
     # node
+
+    # python
+    if ( which node 1>/dev/null)
+    then
+	    echo "node installed"
+    else
+	    echo "-- Installing node......"
+	    apt install -y nodejs
+    fi
     # npm
+    # python
+    if ( which npm 1>/dev/null)
+    then
+	    echo "npm installed"
+    else
+	    echo "-- Installing npm....."
+	    apt install -y npm
+    fi
+
+    # install nvim
+    if ( which nvim 1> /dev/null )
+    then
+	    echo "Nvim available"
+    else
+	    echo "-- Installing nvim......"
+	    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+	    tar xzf nvim-linux64.tar.gz
+	    sudo mv nvim-linux64 /usr/local/nvim
+	    sudo ln -s /usr/local/nvim/bin/nvim /usr/bin/nvim
+    fi
 
     # install rg to be used by telocsocpe
-    echo "-- Installing rg......"
-    #apt install -y ripgrep
+    if ( which rg 1>/dev/null)
+    then
+	    echo "rg installed"
+    else
+	    echo "-- Installing rg......"
+	    apt install -y ripgrep
+    fi
 
     # install eslint
-    npm install -g eslint
-
+    #npm install -g eslint
 }
 dependencies
